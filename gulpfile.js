0583c9226eac53364198ab439e2b96755e73d78b
Aleksei Ivshin <Aleksei_Ivshin@epam.com>
2015-11-17 18:24:21 +0400

    Adds Gulp SASS

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
+'use strict';
 var gulp = require('gulp');
 var webserver = require('gulp-webserver');
+var sass = require('gulp-sass');
 
 gulp.task('minify', function() {
   return gulp.src('client/js/**/*.js')
@@ -7,7 +9,7 @@ gulp.task('minify', function() {
 });
 
 gulp.task('copySrc2Dist', function() {
-  return gulp.src('./src/**/*')
+  return gulp.src(['./src/**/*','!./src/sass','!./src/sass/**'])
     .pipe(gulp.dest('dist'))
 });
 
@@ -20,4 +22,14 @@ gulp.task('webserver', ['copySrc2Dist'], function() {
       directoryListing: true,
       open: 'http://localhost:8000/views/Pool.html'
 	}));
+});
+
+gulp.task('sass', function () {
+  gulp.src('./src/sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./dist/styles'));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./src/sass/**/*.scss', ['sass']);
 });
